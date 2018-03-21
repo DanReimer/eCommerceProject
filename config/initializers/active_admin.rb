7a167d0c57d0a4cc3af574bd0e00798a6274049f
DanReimer <dajudoguy@gmail.com>
2018-03-21 14:50:21 -0500

    Disabled active admin login for now. Will reenable it when users can login.

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -55,6 +55,7 @@ ActiveAdmin.setup do |config|
   # This setting changes the method which Active Admin calls
   # within the application controller.
   # config.authentication_method = :authenticate_admin_user!
+  config.authentication_method = false # TODO: turn authentication back on
 
   # == User Authorization
   #
@@ -87,6 +88,7 @@ ActiveAdmin.setup do |config|
   # This setting changes the method which Active Admin calls
   # (within the application controller) to return the currently logged in user.
   # config.current_user_method = :current_admin_user
+  config.current_user_method = false # TODO: turn authentication back on
 
   # == Logging Out
   #
