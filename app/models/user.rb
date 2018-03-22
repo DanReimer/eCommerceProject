77a5b07b5b0f4ceb2bed29df744c724723452828
DanReimer <dajudoguy@gmail.com>
2018-03-22 13:44:59 -0500

    Made Province optional and gave is_admin a default value.

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,11 +9,12 @@ class User < ApplicationRecord
   # t.datetime "updated_at", null: false
   # t.index ["province_id"], name: "index_users_on_province_id"
 
-  belongs_to :province
+  belongs_to :province, optional: true
+
+  # Username is case insensitive TODO: not sure if I want this
+  # before_save { self.username.downcase! }
 
   validates :username, presence: true, uniqueness: { case_sensitive: false }, length: { maximum: 50 }
-
-  # Username is case insensitive
-  before_save { self.username.downcase! }
   has_secure_password
+  validates :password, presence: true, length: { minimum: 8 }
 end
