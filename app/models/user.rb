45b4c9da8067796238b081947fac900b3f7aa803
DanReimer <dajudoguy@gmail.com>
2018-03-22 10:14:46 -0500

    Added username validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,19 @@
 class User < ApplicationRecord
+  # t.string "username"
+  # t.string "password_digest"
+  # t.boolean "is_admin?"
+  # t.string "street"
+  # t.string "city"
+  # t.integer "province_id"
+  # t.datetime "created_at", null: false
+  # t.datetime "updated_at", null: false
+  # t.index ["province_id"], name: "index_users_on_province_id"
+
   belongs_to :province
+
+  validates :username, presence: true, uniqueness: { case_sensitive: false }, length: { maximum: 50 }
+
+  # Username is case insensitive
+  before_save { self.username.downcase! }
+  has_secure_password
 end
