2b6d4a1525b8031dde5c22bb1ac1c948970a3dca
DanReimer <dajudoguy@gmail.com>
2018-03-22 16:33:23 -0500

    Created session controller for facilitating logging in.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+
   root 'static_pages#index'
 
-  get 'static_pages/about'
+  # get 'static_pages/about'
 
   get '/signup', to: 'users#new'
   resources :users
