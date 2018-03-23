e86ba17044b853270ca557f985dd953b2ebcd25e
DanReimer <dajudoguy@gmail.com>
2018-03-23 13:38:11 -0500

    Had to add additional has_many like with products.

--- app/models/colour.rb
+++ app/models/colour.rb
@@ -1,6 +1,7 @@
 class Colour < ApplicationRecord
   has_many :order_items
   has_many :products, through: :product_colours
+  has_many :product_colours
 
   VALID_COLOUR_HEXCODE = /\A#[0-9A-Fa-f]{6}\z/
   validates :hexcode, presence: true, format: { with: VALID_COLOUR_HEXCODE }
