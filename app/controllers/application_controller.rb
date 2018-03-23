56318dc477514a0d170b1e8404989c34ceed3ad5
DanReimer <dajudoguy@gmail.com>
2018-03-23 13:36:20 -0500

    Added colours to active admin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,14 +7,11 @@ class ApplicationController < ActionController::Base
   end
 
   def access_denied(exception)
-    redirect_to root_path, alert: exception.message
+    flash[:warning] = exception.message
+    redirect_to root_path
   end
 
   def authenticate_admin_user!
     logged_in? ? current_user.admin : false
-  end
-
-  def current_admin_user
-
   end
 end
