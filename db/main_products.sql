d0d54e6947d86bd0453c3a459209414e7ad98431
DanReimer <dajudoguy@gmail.com>
2018-04-02 23:33:18 -0500

    Created order states.

---
+++ db/main_products.sql
@@ -0,0 +1,57 @@
+INSERT INTO products (id, name, price, description, category_id, created_at, updated_at, user_id) VALUES (1, 'Manila Folder (Classic)', 0.85, 'Our classic folder, made of premium manila paper, has been a staple of all file storage solutions since our establishment in 1998. The sturdy stock will resist wear and tear for years. Experience the Award Winning paper file storage solution now!
+
+Comes in sets of 3.
+
+Features:
+<ul>
+	<li>Capacity of up to 173 pieces of paper!</li>
+	<li>True manila stock in a variety of colours</li>
+	<li>1/3 cut tabs for labeling purposes</li>
+</ul>', 1, '2018-03-23 19:22:47.402114', '2018-03-31 04:09:25.177697', null);
+INSERT INTO products (id, name, price, description, category_id, created_at, updated_at, user_id) VALUES (2, 'Manila Folder (Deluxe)', 6.42, 'This DELUXE manila folder has all the fixings. Professional grade stock? Check! Eye-pleasing etched design? Check! A whopping 1/8 cut? Check! Splurge now! Get this amazing solution for all your storage woes.
+
+Comes with a 2-year limited warranty!
+
+', 1, '2018-03-26 17:49:34.564221', '2018-03-26 17:55:06.779475', null);
+INSERT INTO products (id, name, price, description, category_id, created_at, updated_at, user_id) VALUES (3, 'File Pocket™', 2, 'A pocket of premium synthetic materials. File Pockets™ are a no fuss solution to paper storage AND organization, just slip your File Pocket™ into another compatible product for instant separation of important files. 
+
+Guaranteed compatibility with all P4P folding products!
+
+Comes with selection of adhesive labels for greater organizational magic!', 2, '2018-03-26 18:01:48.092847', '2018-03-31 03:59:09.660183', null);
+INSERT INTO products (id, name, price, description, category_id, created_at, updated_at, user_id) VALUES (4, 'Presentation Folder', 10.75, 'This folder isn''t like the others. Inside of its folds lie two convenient pockets!  These deep pockets keep contents secure and are perfect for organizing special projects. Keep your documents in this portable, easy-access location.
+
+Comes in a pack of 10 folders.', 2, '2018-03-26 18:06:34.287614', '2018-03-31 03:57:05.243416', null);
+INSERT INTO products (id, name, price, description, category_id, created_at, updated_at, user_id) VALUES (5, 'Retro Presentation Folder', 16.99, 'This folder isn''t like the others. Inside of its folds lie two convenient pockets! These deep pockets keep contents secure and are perfect for organizing special projects. Keep your documents in this portable, easy-access location.
+
+These presentation folders have a variety of retro designs. One order of this item contains 10 presentation folders, each with a unique retro design!', 2, '2018-03-26 18:10:51.345165', '2018-03-31 03:47:15.861146', null);
+INSERT INTO products (id, name, price, description, category_id, created_at, updated_at, user_id) VALUES (6, 'Duotang', 4.88, '<ul>
+	<li>Built-in metal fasteners and eyelets secure papers in a bound format</li>
+	<li>Letter size</li>
+	<li>10% post-consumer recycled conten</li>
+	<li>Holds up to 12.7mm of 3-hole punched paper</li>
+</ul>
+
+Comes in a pack of 6 duotangs.', 3, '2018-03-26 18:16:41.297076', '2018-03-31 03:44:50.604677', null);
+INSERT INTO products (id, name, price, description, category_id, created_at, updated_at, user_id) VALUES (7, 'Manila Folder (large)', 57.85, 'Novelty sized folder. Show your P4P spirit! After order you will be contacted by letter to provide embossing details.
+May incur additional shipping charges.
+
+<ul>
+	<li>1 meter tall</li>
+	<li>True manila stock in a variety of colours</li>
+	<li>Custom embossed front (up to 30 characters)</li>
+</ul>', 1, '2018-03-26 18:20:50.537380', '2018-03-31 03:43:40.011244', null);
+INSERT INTO products (id, name, price, description, category_id, created_at, updated_at, user_id) VALUES (8, 'Paper for Paper File Storage Special - sample pack', 20, 'Want to know what we''re all about here at Paper for Paper? Check out this variety pack for a smorgasbord of file storage options!
+
+Includes:
+<ul>
+	<li>6 Classic Manila folders - 2 in Green, 2 in White, and 2 in Black</li>
+	<li>2 File Pockets™</li>
+	<li>2 Presentation Folders</li>
+	<li>2 duotangs</li>
+</ul>', 4, '2018-03-26 18:25:53.517662', '2018-03-31 03:35:52.152717', null);
+INSERT INTO products (id, name, price, description, category_id, created_at, updated_at, user_id) VALUES (9, 'Ring folder', 12.12, 'This fancy folder secures paper with ring-like fasteners. A truly revolutionary product that can store greater amounts of paper than other file storage solutions.', 3, '2018-03-26 18:28:16.246928', '2018-03-31 03:30:59.740726', null);
+INSERT INTO products (id, name, price, description, category_id, created_at, updated_at, user_id) VALUES (10, 'FileVelope', 5.05, 'An envelope for your documents! Sturdy paper envelope that can fit several letter size papers UNFOLDED. Reduce unsightly paper creases caused by smaller envelopes.
+
+Pack of 20
+', 2, '2018-03-26 18:31:42.281993', '2018-03-29 19:15:38.339637', null);
+INSERT INTO products (id, name, price, description, category_id, created_at, updated_at, user_id) VALUES (13, 'Moment Pocket Folder', 25.11, 'This utilitarian knife is an everyday carry made to compliment your larger, more task-specific hunting knives. The slim profile is portable at only 2.5 ounces, while the rubber inlay handle and frame lock design keep the 3 inch drop point blade steady.', 1, '2018-03-31 04:06:21.732596', '2018-03-31 04:06:21.758871', null);
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_03_29_152328) do
+ActiveRecord::Schema.define(version: 2018_04_03_043047) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -73,6 +73,12 @@ ActiveRecord::Schema.define(version: 2018_03_29_152328) do
     t.index ["product_id"], name: "index_order_items_on_product_id"
   end
 
+  create_table "order_states", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "orders", force: :cascade do |t|
     t.integer "user_id"
     t.decimal "subtotal"
@@ -81,6 +87,8 @@ ActiveRecord::Schema.define(version: 2018_03_29_152328) do
     t.decimal "hst_amount"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "order_state_id"
+    t.index ["order_state_id"], name: "index_orders_on_order_state_id"
     t.index ["user_id"], name: "index_orders_on_user_id"
   end
 
