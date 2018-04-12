b674d29ab1808a1a89994d5c37872ab282b8feb4
DanReimer <dajudoguy@gmail.com>
2018-04-12 11:41:21 -0500

    Removed auto-generated docs

--- config/routes.rb
+++ config/routes.rb
@@ -15,8 +15,5 @@ Rails.application.routes.draw do
   post '/signup', to: 'users#create'
   resources :users, only: %i[show edit update]
 
-  # get 'static_pages/about'
-
   ActiveAdmin.routes(self)
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
